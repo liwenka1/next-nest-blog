3d640022792d1461a268105f981366a79912e97d
liwenkai <2020583117@qq.com>
2024-05-31 22:18:48 +0800

    fix: 后端添加拦截器

--- packages/serve/src/app.module.ts
+++ packages/serve/src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { PostModule } from './post/post.module'
+import { TransformInterceptor } from './config/transform.interceptor'
+import { ErrorsInterceptor } from './config/errors.interceptor'
 
 @Module({
   imports: [UserModule, PostModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ErrorsInterceptor
+    },
+    AppService
+  ]
 })
 export class AppModule {}
