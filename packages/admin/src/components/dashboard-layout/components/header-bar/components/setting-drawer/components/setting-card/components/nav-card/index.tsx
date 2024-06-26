9a9dda4e617ef81320c26fc22839f973b6ca4aa0
liwenkai <2020583117@qq.com>
2024-06-26 23:46:04 +0800

    fix: nav-card相关逻辑

--- packages/admin/src/components/dashboard-layout/components/header-bar/components/setting-drawer/components/setting-card/components/nav-card/index.tsx
+++ packages/admin/src/components/dashboard-layout/components/header-bar/components/setting-drawer/components/setting-card/components/nav-card/index.tsx
@@ -5,7 +5,17 @@ import { useSettingStore } from '@/stores'
 import { ThemeNavLatout } from '#/enum'
 
 const NavCard = () => {
-  const { themePresets, setSetting } = useSettingStore()
+  const { themePresets, themeNavLatout, collapsed, setSetting } = useSettingStore()
+  const themeNavLatoutStyle = (latout: ThemeNavLatout, isCollapsed?: boolean) => {
+    if (
+      (latout === ThemeNavLatout.Horizontal && latout === themeNavLatout) ||
+      (latout === themeNavLatout && isCollapsed === collapsed)
+    ) {
+      return { backgroundColor: themePresetsColors[themePresets] }
+    } else {
+      return { backgroundColor: '#919EAB' }
+    }
+  }
   const NavCardList = [
     {
       title: 'Layout',
@@ -21,21 +31,21 @@ const NavCard = () => {
             <div className="flex h-full w-[32px] flex-col gap-[4px] p-[6px]">
               <div
                 className="h-[10px] w-[10px] flex-shrink-0 rounded-full"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, false)}
               />
               <div
                 className="h-[4px] w-full flex-shrink-0 rounded opacity-50"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, false)}
               />
               <div
                 className="h-[4px] max-w-[14px] flex-shrink-0 rounded opacity-20"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, false)}
               />
             </div>
             <div className="h-full w-full flex-1 flex-grow p-[6px]">
               <div
                 className="h-full w-full rounded opacity-20"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, false)}
               />
             </div>
           </ShodowButton>
@@ -46,21 +56,21 @@ const NavCard = () => {
             <div className="flex h-[22px] w-full items-center gap-[4px] p-[6px]">
               <div
                 className="h-[10px] w-[10px] flex-shrink-0 rounded-full"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Horizontal)}
               />
               <div
                 className="h-[4px] w-[16px] flex-shrink-0 rounded opacity-50"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Horizontal)}
               />
               <div
                 className="h-[4px] w-full max-w-[10px] flex-shrink-0 rounded opacity-20"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Horizontal)}
               />
             </div>
             <div className="h-full w-full flex-1 flex-grow p-[6px]">
               <div
                 className="h-full w-full rounded opacity-20"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Horizontal)}
               />
             </div>
           </ShodowButton>
@@ -74,21 +84,21 @@ const NavCard = () => {
             <div className="flex h-full flex-col gap-[4px] p-[6px]">
               <div
                 className="h-[10px] w-[10px] flex-shrink-0 rounded-full"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, true)}
               />
               <div
                 className="h-[4px] w-full flex-shrink-0 rounded opacity-50"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, true)}
               />
               <div
                 className="h-[4px] w-full flex-shrink-0 rounded opacity-20"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, true)}
               />
             </div>
             <div className="h-full w-full flex-1 flex-grow p-[6px]">
               <div
                 className="h-full w-full rounded opacity-20"
-                style={{ backgroundColor: themePresetsColors[themePresets] }}
+                style={themeNavLatoutStyle(ThemeNavLatout.Inline, true)}
               />
             </div>
           </ShodowButton>
